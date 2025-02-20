ba8478bd0e66b14b0098661c50aa1a044e2c5744
Adam Cahan <adam@codeplatoon.org>
2025-02-19 22:14:38 -0600

    solution, passes tests

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-def hight(words: str) -> str:
-    pass
+def evaluate_word(word: str) -> int:
+    score = 0
+    for ltr in word:
+        score += ord(ltr) - 96
+    return score
+
+
+def high(words: str) -> str:
+    list_of_words = words.split()
+    # print(list_of_words)
+    highest_score_word = {
+        "word": list_of_words[0],
+        "score": evaluate_word(list_of_words[0]),
+    }
+    for word in list_of_words[1:]:
+        score = evaluate_word(word)
+        if highest_score_word.get("score") < score:
+            highest_score_word["word"] = word
+            highest_score_word["score"] = score
+        # print(highest_score_word)
+    return highest_score_word.get("word")
